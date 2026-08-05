a1a611b2af9c9034cd378a995f3db6c939aef3ba
ramiro brandon <ramitpr53@gmail.com>
2026-08-04 23:02:47 -0400

    restructed project of siat

--- apps/backend/src/point-of-sale/dto/update-point-of-sale.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePointOfSaleDto } from './create-point-of-sale.dto';
-
-export class UpdatePointOfSaleDto extends PartialType(CreatePointOfSaleDto) {}
